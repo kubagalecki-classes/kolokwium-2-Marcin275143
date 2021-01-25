475bb17b82f3510d6fc118ccdbe6124fb410156b
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-25 18:32:46 +0100

    Update zad4.hpp

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -5,3 +5,10 @@
 #include <vector>
 
 // tutaj funkcja jedzOstatnieWarzywa
+
+
+void jedzOstatnieWarzywa(const vector <Warzywo> &W, unsigned& N, ostream& O)
+{
+	if (N >= W.size()) throw logic_error("To jest wyjatek");
+	for (vector <Warzywo>::iterator it = W.rbegin(); it != W.rend(); it++) { it + 1 -> opis(O); }
+}
