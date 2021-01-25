7e230693fb88f34c9292468117c2b9c966e794df
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-25 18:23:38 +0100

    Update zad3.hpp

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -5,3 +5,15 @@
 #include <functional>
 
 // tutaj szablon gotujZupe
+
+template <typename T>
+unsigned gotujZupe(const Warzywo& w, const T& t)
+{
+	return ((t.gotujZupe(w)). ^ 2);
+}
+
+void jedzOstatnieWarzywa(const vector <Warzywo> &W, unsigned& N, ostream& O)
+{
+	if (N >= W.size()) throw logic_error("To jest wyjatek");
+	for (vector <Warzywo>::iterator it = W.rbegin(); it != W.rend(); it++) { it + 1 -> opis(O); }
+}
