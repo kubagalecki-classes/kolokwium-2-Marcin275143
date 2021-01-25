c2572f075d8d21e5df07a06c047ca999b0b33779
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-25 18:14:38 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,4 +6,32 @@
 #include <sstream>
 #include <string>
 
+using namespace std;
+
+class Warzywo
+{
+public:
+
+	Warzywo(const string& N, const double& C, const int& K) : nazwa(N), cena(C), kolor(K) { ++liczba_warzyw; };
+
+	static unsigned getVeg() { return liczba_warzyw; }
+
+	void opis(ostream& os) const { os << nazwa << ": " << cena << ", " << kolor << endl; }
+
+	string get_nazwa() { return nazwa; }
+
+	~Warzywo() { --liczba_warzyw; }
+
+
+
+private:
+
+	string nazwa;
+	double cena;
+	int kolor;
+	inline static unsigned liczba_warzyw;
+
+};
+
+unsigned Warzywo::liczba_warzyw = 0;
 // tutaj klasa Warzywo
