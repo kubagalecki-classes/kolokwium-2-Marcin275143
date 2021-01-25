4e69e0b1012e1f952409baf5fc5c0f7e5d701163
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-25 19:09:26 +0100

    Update zad5.hpp

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -4,4 +4,11 @@
 #include <deque>
 #include <vector>
 
+template <typename typ>
+bool liczKolejneWarzywa(typ poczatek, typ koniec, unsigned n, const string &s)
+{
+	Warzywo w = Warzywo(s, 40, 4);
+	if (search_n(poczatek, koniec, n, w, Funktor) == poczatek) ? true : false;
+}
+
 // tutaj algorytm liczKolejneWarzywa
