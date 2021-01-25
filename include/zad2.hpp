e473e051c280d4d8c27bd59f15fd3fa65c872456
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-25 18:21:37 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -3,4 +3,28 @@
 #include "catch.hpp"
 #include "chrup.hpp"
 
+using namespace std;
+
+class Ogorek
+{
+public:
+
+	virtual string chrup() = 0;
+
+};
+
+class Zielony : public Ogorek
+{
+
+	virtual string chrup() override { chrupZielony(); }
+
+};
+
+class Kiszony : public Ogorek
+{
+
+	virtual string chrup() override { chrupKiszony(); }
+
+};
+
 // tutaj klasy Ogorek, Zielony i Kiszony
