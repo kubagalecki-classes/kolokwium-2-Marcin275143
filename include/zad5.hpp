1251aa7fbb468f9caa14fc39eb35040a0e01ba60
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-25 19:42:13 +0100

    Update zad5.hpp

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -4,6 +4,8 @@
 #include <deque>
 #include <vector>
 
+bool Funktor(Warzywo& w1, Warzywo& w2) { if (w1.get_nazwa() == w2.get_nazwa()) return true; }
+
 template <typename typ>
 bool liczKolejneWarzywa(typ poczatek, typ koniec, unsigned n, const string &s)
 {
