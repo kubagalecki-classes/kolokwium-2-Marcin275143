425ba2e9e149fc56baa2e9d5425b1a99f7c5ec15
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-25 18:32:19 +0100

    Update zad3.hpp

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -11,9 +11,3 @@ unsigned gotujZupe(const Warzywo& w, const T& t)
 {
 	return ((t.gotujZupe(w)). ^ 2);
 }
-
-void jedzOstatnieWarzywa(const vector <Warzywo> &W, unsigned& N, ostream& O)
-{
-	if (N >= W.size()) throw logic_error("To jest wyjatek");
-	for (vector <Warzywo>::iterator it = W.rbegin(); it != W.rend(); it++) { it + 1 -> opis(O); }
-}
